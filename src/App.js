4e4aab74e2cc39f4361202f8e2e6bd6e845178f5
kolamaja <mikin99@poczta.onet.pl>
2022-11-12 22:06:42 +0100

    footer work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import NavBar from "./components/NavBar";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaEnvelope} from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 function App() {
 	return (
-		<div className="flex flex-col justify-between h-screen ">
+		<div className='flex flex-col justify-between h-screen '>
 			<NavBar />
 
 			<div className='container mx-auto '>
@@ -32,30 +32,49 @@ function App() {
 					</div>
 				</div>
 			</div>
-			<section>
-
-			</section>
+			<section></section>
 			<footer className='bg-elephant-500'>
-				<div className='container flex flex-row items-center py-12 px-8 mx-auto mt md:space-y-0'>
-						<div className='flex flex-col space-y-3 text-2xl text-clay-500'>
-							<a className="hover:text-clay-400" href='www.google.pl'><FaLinkedin/> </a>
-							<a className="hover:text-clay-400" href='www.google.pl'><FaGithub/></a>
-							<a className="hover:text-clay-400" href='www.google.pl'><FaEnvelope/></a>
-						</div>
-						<div className="flex flex-col space-y-3 px-2 text-almostWhite-500">
-						<h1 className='cursor-default'>
-							Linkedin
-						</h1>
-						<h1 className='cursor-default'>
-							GitHub
-						</h1>
-						<h1 className='cursor-default'>
-							Email
-						</h1>
+				<div className='container flex flex-row justify-between  py-12 px-8 mx-auto mt md:space-y-0'>
+					<div className="flex items-center">
+					<div className='flex flex-col space-y-3 text-2xl  text-clay-500'>
+						<a className='hover:text-clay-400' href='www.google.pl'>
+							<FaLinkedin />
+						</a>
+						<a className='hover:text-clay-400' href='www.google.pl'>
+							<FaGithub />
+						</a>
+						<a className='hover:text-clay-400' href='www.google.pl'>
+							<FaEnvelope />
+						</a>
+					</div>
+					<div className='flex flex-col space-y-3 px-2 text-almostWhite-500'>
+						<h1 className='cursor-default'>Linkedin</h1>
+						<h1 className='cursor-default'>GitHub</h1>
+						<h1 className='cursor-default'>Email</h1>
+					</div>
+					<div className='flex flex-col space-y-2 px-6  text-almostWhite-500'>
+						<a className='hover:text-almostWhite-600' href='www.google.pl'>
+							Home
+						</a>
+						<a className='hover:text-almostWhite-600' href='www.google.pl'>
+							About me
+						</a>
+						<a className='hover:text-almostWhite-600' href='www.google.pl'>
+							My portfolio
+						</a>
+						<a className='hover:text-almostWhite-600' href='www.google.pl'>
+							Contact
+						</a>
+					</div>
+					</div>
+					<div className="flex flex-col  text-almostWhite-500">
+						<h1 className="text-xl">Keep in touch!</h1>
 						
 
-						</div>
-					
+					</div>
+				</div>
+				<div className='bg-elephant-600 flex flex-col  justify-center text-center'>
+					<p>Copyright &copy; 2022, All Rights Reserved</p>
 				</div>
 			</footer>
 		</div>
