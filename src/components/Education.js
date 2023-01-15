c05b23117ef76d0b152f91f4b3ab6a4f210eba6a
kolamaja <mikin99@poczta.onet.pl>
2023-01-15 20:40:52 +0100

    feat(Education): Mobile version

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,20 +6,20 @@ import KUL from "../img/KUL.png";
 
 export const Education = () => {
   return (
-    <div className="bg-education rounded-b-3xl bg-cover bg-top shadow-md  bg-no-repeat">
+    <div className="bg-education rounded-b-3xl bg-cover bg-top shadow-md lg:pb-0 pb-24  bg-no-repeat">
       <div className="container    mx-auto">
-        <div className="flex flex-col drop-shadow-sm mt-10 pb-10 pt-4 mx-16">
+        <div className="flex flex-col drop-shadow-sm lg:mt-10 pb-10 lg:pt-4 pt-6 mx-16">
           <div className="flex flex-row justify-center p-2">
-            <h1 className="text-6xl text-clay-500"> Education</h1>
+            <h1 className="lg:text-6xl text-3xl text-clay-500"> Education</h1>
           </div>
-          <div className="flex flex-col px-3">
+          <div className="flex flex-col lg:px-3 px-1   text-center ">
             <div className="flex flex-row justify-center pt-3  ">
-              <h1 className="text-base italic relative text-stone-500">
-                <FaQuoteLeft className="absolute -top-3 -left-5 text-base" />
+              <h1 className="lg:text-base text-sm italic relative text-stone-500">
+                <FaQuoteLeft className="absolute -top-3 -left-5 lg:text-base text-xs" />
                 True education is a kind of never ending story — a matter of
                 continual beginnings, of habitual fresh starts, of persistent
                 newness.
-                <FaQuoteRight className="absolute text-base -right-5" />
+                <FaQuoteRight className="absolute lg:text-base text-xs -right-5" />
               </h1>
             </div>
             <div className="flex flex-row pt-4 text-sm text-stone-500  justify-center">
@@ -33,9 +33,9 @@ export const Education = () => {
           </div>
 
           <div className="flex group/all relative flex-row px-2 py-6 justify-evenly items-center">
-            <div className="  group  flex flex-col items-center">
+            <div className="  group  flex flex-col items-center lg:mr-0 mr-2">
               <div
-                className="absolute -top-20 hidden group-hover:inline-flex pl-6 items-center z-40 justify-center
+                className="absolute -top-20 hidden lg:group-hover:inline-flex pl-6 items-center z-40 justify-center
                bg-almostWhite-500 border-2 border-clay-500 shadow-lg shadow-elephant-500"
               >
                 <div className="flex flex-col">
@@ -49,16 +49,16 @@ export const Education = () => {
                 </div>
                 <img className="w-32 h-32 p-3" src={ZST} alt="" />
               </div>
-              <h1 className="text-8xl group-hover/all:scale-110 group-hover/all:text-clay-400 text-clay-500">
+              <h1 className="lg:text-8xl text-6xl lg:group-hover/all:scale-110 lg:group-hover/all:text-clay-400 lg:text-clay-500 text-almostWhite-300">
                 <FaSchool />
               </h1>
-              <h1 className="text-lg text-almostWhite-100 group-hover/all:scale-110 bg-elephant-700 bg-opacity-60 rounded-full my-1 py-1 px-2 ">
+              <h1 className="lg:text-lg text-xs text-center  text-almostWhite-100 lg:group-hover/all:scale-110 bg-elephant-700 bg-opacity-60 rounded-full my-1 py-1 px-2 ">
                 Technical IT school
               </h1>
             </div>
-            <div className="group flex flex-col text-elephant-500 items-center">
+            <div className="group flex flex-col text-elephant-500 lg:ml-0 ml-2 items-center">
               <div
-                className="absolute -top-20 hidden group-hover:inline-flex pl-6 items-center z-40 justify-center
+                className="absolute -top-20 hidden   lg:group-hover:inline-flex pl-6 items-center z-40 justify-center
                bg-almostWhite-500 border-2 border-clay-500 shadow-lg shadow-elephant-500"
               >
                 <div className="flex flex-col">
@@ -72,10 +72,10 @@ export const Education = () => {
                 </div>
                 <img className="w-32 h-32 p-3" src={KUL} alt="" />
               </div>
-              <h1 className="text-8xl group-hover/all:scale-110 group-hover/all:text-clay-400  text-clay-500">
+              <h1 className="lg:text-8xl text-6xl lg:group-hover/all:scale-110 lg:group-hover/all:text-clay-400  lg:text-clay-500 text-almostWhite-300">
                 <MdSchool />
               </h1>
-              <h1 className="text-lg text-almostWhite-100 group-hover/all:scale-110  bg-elephant-700 bg-opacity-60 rounded-full  py-1 px-2">
+              <h1 className="lg:text-lg text-xs text-center text-almostWhite-100 lg:group-hover/all:scale-110  bg-elephant-700 bg-opacity-60 rounded-full  py-1 px-2">
                 Bachelor's degree in IT
               </h1>
             </div>
