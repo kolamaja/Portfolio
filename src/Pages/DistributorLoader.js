20353328d7c3c1cb7a6ee8d79463cc3c8f86fe67
kolamaja <mikin99@poczta.onet.pl>
2023-01-16 22:13:45 +0100

    feat(DistributorLoader): Add temporary mobile version

--- src/Pages/DistributorLoader.js
+++ src/Pages/DistributorLoader.js
@@ -6,10 +6,10 @@ import dist4 from "../img/dist4.PNG";
 import { Link } from "react-router-dom";
 export const DistributorLoader = () => {
   return (
-    <div className="container mx-auto ">
+    <div className="container mx-auto h-screen">
       <Link to={"/portfolio"}><button className="bg-clay-500 p-2 m-4 rounded-full text-almostWhite-500">Go back</button></Link>
-      <div className="flex flex-col my-10 justify-center items-center">
-        <h1 className="text-2xl my-6">Work in progres</h1>
+      <div className="flex flex-col mx-2 my-10 justify-center items-center">
+        <h1 className="text-2xl  my-6">Work in progres</h1>
         <p className="max-w-md text-lg">
           This application is used to convert an xml file to json and display
           all the data it contains. It is possible to sort the results and enter
@@ -19,22 +19,22 @@ export const DistributorLoader = () => {
         <h1 className="text-2xl my-6">Below are some screenshots from the app</h1>
         <div className="flex flex-row justify-center flex-wrap ">
           <img
-            className=" max-w-6xl shadow-md p-4 my-4 "
+            className=" max-w-sm lg:max-w-6xl shadow-md p-4 my-4 "
             src={dist1}
             alt={dist1}
           ></img>
           <img
-            className=" max-w-6xl shadow-md p-4 my-4  "
+            className=" max-w-sm lg:max-w-6xl shadow-md p-4 my-4  "
             src={dist2}
             alt={dist2}
           ></img>
           <img
-            className=" max-w-6xl shadow-md p-4 my-4  "
+            className=" max-w-sm lg:max-w-6xl shadow-md p-4 my-4  "
             src={dist3}
             alt={dist3}
           ></img>
           <img
-            className=" max-w-6xl shadow-md p-4 my-4  "
+            className=" max-w-sm lg:max-w-6xl shadow-md p-4 my-4  "
             src={dist4}
             alt={dist4}
           ></img>
